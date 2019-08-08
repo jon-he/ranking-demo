6c03ec606b263f32e1e9495e9d8f677912d85821
jon.he <jon.he@imaginato.com>
2019-08-08 17:38:42 +0800

    rank demo

--- src/Components/ListItem/styles.js
+++ src/Components/ListItem/styles.js
@@ -6,6 +6,9 @@ const RankItemBox = styled.div`
   align-items: center;
   justify-content: space-between;
   font-size: 14px;
+  background-color: #d3d7d4;
+  border-radius: 5px;
+  padding: 5px 14px;
 
   .boxLeft {
     display: flex;
--- src/Containers/Rank/styles.js
+++ src/Containers/Rank/styles.js
@@ -11,10 +11,11 @@ const ContainerBox = styled.div`
 
 const ListBox = styled.div`
   position: relative;
-  margin: 20px auto;
+  margin: 5% auto;
   width: 280px;
-  @media screen and (min-width: 350px) {
-    width: 70%;
+  @media screen and (min-width: 320px) {
+    margin: 20px auto;
+    width: 90%;
     max-width: 480px;
   }
 `;
@@ -23,11 +24,9 @@ const BoxItem = styled.div`
   position: absolute;
   top: ${props => props.sort * boxItemHeight}px;
   left: 0;
+  right: 0;
   transition: top 0.3s ease 0s;
   width: 100%;
-  background-color: #d3d7d4;
-  border-radius: 5px;
-  padding: 5px 14px;
 `;
 
 export { ContainerBox, ListBox, BoxItem };
